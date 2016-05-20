9b5a3c41ac2553b8d039566d3fe42a13c81fb416
Mridul <Mridul>
2016-05-21 01:57:51 +0530

    correction in last app.js file for app listener

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(port, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
 });
 

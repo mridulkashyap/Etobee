ba5c305a71f21ed82665d1eabe04c231c9b86bb6
Mridul <Mridul>
2016-05-20 23:42:20 +0530

    updated the config file with env variable

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgre@localhost:5432/todo';  //postgres://YOURUSER:YOURPASSWORD@localhost/dev
+var connectionString = process.env.OPENSHIFT_POSTGRE_DB_URL || 'postgres://postgres:postgre@localhost:5432/todo';  //postgres://YOURUSER:YOURPASSWORD@localhost/dev
+//var connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgre@localhost:5432/todo';  //postgres://YOURUSER:YOURPASSWORD@localhost/dev
 
 module.exports = connectionString;

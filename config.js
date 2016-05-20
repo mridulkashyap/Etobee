c12afedc8740fda49920e20b8bb4cfad0c761aa2
Mridul <Mridul>
2016-05-21 00:28:21 +0530

    trying to add the schema creation in config to setup on first run

--- config.js
+++ config.js
@@ -1,4 +1,28 @@
 var connectionString = process.env.OPENSHIFT_POSTGRESQL_DB_URL || 'postgres://postgres:postgre@localhost:5432/todo';  //postgres://YOURUSER:YOURPASSWORD@localhost/dev
 //var connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgre@localhost:5432/todo';  //postgres://YOURUSER:YOURPASSWORD@localhost/dev
 
+pg.connect(connectionString, function(err, client, done) { // connect to postgres db
+    if (err)
+        console.log('Error while connecting: ' + err); 
+    client.query('CREATE DATABASE ' + dbName, function(err) { // create user's db
+        if (err) 
+            console.log('ignoring the error'); // ignore if the db is there
+        client.end(); // close the connection
+        });
+    });
+	
+	// create a new connection to the new db to start creating tables
+	pg.connect(conStringPost, function(err, clientOrg, done) {
+		// create the table
+		var query = client.query(
+							'CREATE TABLE IF NOT EXISTS items( ' +
+								'id SERIAL PRIMARY KEY, ' +
+								'text VARCHAR(40) not null, ' +
+								'complete BOOLEAN' +
+								')'
+							);
+		query.on('end', function() { client.end(); });
+	});
+});
+
 module.exports = connectionString;
